1a35656494e69462ef39ea6bdf57c08d910c49ac
Bryan Tapia <110837963+Brynta2001@users.noreply.github.com>
2023-03-07 20:51:07 -0500

    Update DashboardController.java

--- src/main/java/controlador/DashboardController.java
+++ src/main/java/controlador/DashboardController.java
@@ -18,49 +18,53 @@ import utilidades.Mes;
 @WebServlet("/DashboardController")
 public class DashboardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public DashboardController() {
-        super();
-    }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public DashboardController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		ruteador(request, response);
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		ruteador(request, response);
 	}
-	
-	private void ruteador(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String ruta = (request.getParameter("ruta")==null?"home":request.getParameter("ruta"));
-		
-		switch(ruta) {
+
+	private void ruteador(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String ruta = (request.getParameter("ruta") == null ? "home" : request.getParameter("ruta"));
+
+		switch (ruta) {
 		case "home":
 			showHome(request, response);
 			break;
 		case "ver":
 			showDashboard(request, response);
 			break;
-		
-		case "login":
-			showDashboard(request, response);
-			break;
-	}
-	}
-	
-	private void showHome(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/jsp/home.jsp").forward(request, response);
-		
-	}
-	private void showDashboard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int mes = LocalDate.now().getMonth().getValue();
-		
-		if(request.getParameter("mes")!=null) {
-			mes = Integer.parseInt( request.getParameter("mes"));
 		}
-		
+	}
+
+	private void showHome(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/jsp/home.jsp").forward(request, response);
+
+	}
+
+	private void showDashboard(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int mes = LocalDate.now().getMonth().getValue();
+
+		if (request.getParameter("mes") != null) {
+			mes = Integer.parseInt(request.getParameter("mes"));
+		}
+
 		List<CuentaDTO> conjuntoingresos = DAOFactory.getFactory().getCuentaDAO().getConsolidadoCuentasIngreso(mes);
 		List<CuentaDTO> conjuntogastos = DAOFactory.getFactory().getCuentaDAO().getConsolidadoCuentsEgreso(mes);
 		List<Cuenta> conjuntoingresogasto = DAOFactory.getFactory().getCuentaDAO().getConsolidadoCuentasIngresoEgreso();
-		
+
 		request.setAttribute("messeleccionado", mes);
 		request.setAttribute("meses", Mes.getMeses());
 		request.setAttribute("ingresos", conjuntoingresos);
