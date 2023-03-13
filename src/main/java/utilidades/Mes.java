8a029fe75b31dab74d7eb345be5b43c4c16684f7
Mateo Díaz <mateo.diaz@epn.edu.ec>
2023-03-13 11:27:45 -0500

    Documentacion JavaDoc

--- src/main/java/utilidades/Mes.java
+++ src/main/java/utilidades/Mes.java
@@ -2,35 +2,67 @@ package utilidades;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Clase que contiene los meses del año
+ */
 public class Mes {
+	/**
+	 * Numeracion del mes
+	 */
 	private int id;
+	/**
+	 * Nombre del mes
+	 */
 	private String nombre;
 	
+	/**
+	 * Constructor vacio
+	 */
 	public Mes() {}
 
+	/**
+	 * Constructor con parametros
+	 * @param id Numero de mes
+	 * @param nombre Nombre del mes
+	 */
 	public Mes(int id, String nombre) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 	}
 
+	/**
+	 * @return Numero del mes
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @param id Numero del mes
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return Nombre del mes
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * @param nombre Nombre del mes
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
+	/**
+	 * Metodo que devuelve todos los meses del año
+	 * @return Lista con objejtos tipo Mes
+	 */
 	public static List<Mes> getMeses() {
 		List<Mes> meses = new ArrayList<Mes>();
 		meses.add(new Mes(1, "Enero"));
